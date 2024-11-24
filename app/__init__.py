a2693512db14c065f4652aafa72101bf7eb1bcf5
Joseph Masters <jsbmasters@gmail.com>
2024-11-24 15:56:10 -0600

    Fix ImportError for create_app

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    # Import routes
+    # Import and register routes
     from .routes import bp
     app.register_blueprint(bp)
 
